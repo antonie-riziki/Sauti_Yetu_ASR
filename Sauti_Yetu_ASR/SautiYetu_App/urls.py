c43eb493262f717c4a5ff00c3e59ad57a238bba0
antonie-riziki <antonriziki@gmail.com>
2025-11-16 21:56:41 +0300

    updated the whole system UI templates

--- Sauti_Yetu_ASR/SautiYetu_App/urls.py
+++ Sauti_Yetu_ASR/SautiYetu_App/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [    
     path('', views.home, name='home'),
+    path('community/', views.community, name='community'),
+    path('nearby/', views.nearby, name='nearby'),
+    path('patient-dashboard/', views.patient_dashboard, name='patient_dashboard'),
+    path('patient-session/', views.patient_sessions, name='patient_sessions'),
+    path('patients/', views.patients, name='patients'),
+    path('sessions/', views.sessions, name='session'),
+    path('therapy-session/', views.therapy_session, name='therapist_session'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
 ]
--- Sauti_Yetu_ASR/SautiYetu_App/views.py
+++ Sauti_Yetu_ASR/SautiYetu_App/views.py
@@ -3,3 +3,52 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def community(request):
+    return render(request, 'community.html')
+
+
+def nearby(request):
+    return render(request, 'nearby.html')   
+
+
+def patient_dashboard(request):
+    return render(request, 'patient-dashboard.html')
+
+
+def patient_sessions(request):
+    return render(request, 'patient-session.html')
+
+
+def patients(request):
+    return render(request, 'patients.html')
+
+
+def therapy_session(request):
+    return render(request, 'therapy-session.html')
+
+
+def sessions(request):
+    return render(request, 'sessions.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')   
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+# def therapist_dashboard(request):
+#     return render(request, 'therapist-dashboard.html')
+
+
+# def therapist_session(request):
+#     return render(request, 'therapist-session.html')
+
+
+# def therapist_sessions(request):
+#     return render(request, 'therapist-sessions.html')
+
